cb63f9faa4a355b34be12aa7daf2dd6a6706083e
shrikanth385 <letsgo385@gmail.com>
2023-10-14 14:19:20 +0530

    Deleting favorites functionality

--- script.js
+++ script.js
@@ -56,7 +56,6 @@ document.addEventListener("DOMContentLoaded", function () {
         movieInfoContainer.classList.add("scale-up-center");
         const movieHeadingElements = document.createElement("div");
         const movieContext = document.createElement("div");
-        const favoritesButton = document.createElement("button");
         const movieTitleElement = document.createElement("h2");
         const moviePoster = document.createElement("img");
         const moviePlotElement = document.createElement("p");
@@ -77,46 +76,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
         moviePoster.src = data.Poster;
         moviePoster.alt = data.Title;
-        favoritesButton.textContent = "Add to favorites";
-        favoritesButton.style.fontSize = "0.5rem";
-        favoritesButton.classList.add("button-style");
         moviePlotElement.textContent = data.Plot;
         movieCastElement.textContent = `Cast: ${data.Actors}`;
-
-        // Retrieve favorites from local storage
-        const favorites = getFavorites() || [];
-        console.log(favorites);
-        // Check if the current movie is already in favorites
-        const isFavorite = favorites.includes(`${data.Title}|${data.Poster}`);
-
-        if (isFavorite) {
-          favoritesButton.textContent = "Remove from favorites";
-        }
-
-        favoritesButton.addEventListener("click", function () {
-          // Toggle between adding and removing from favorites
-          if (isFavorite) {
-            // Remove from favorites
-            const updatedFavorites = favorites.filter(
-              (fav) => fav !== `${data.Title}|${data.Poster}`
-            );
-
-            setFavorites(updatedFavorites);
-            favoritesButton.textContent = "Add to favorites";
-          } else {
-            // Add to favorites
-            const newFavorite = `${data.Title}|${data.Poster}`;
-            favorites.push(newFavorite);
-            setFavorites(favorites);
-            favoritesButton.textContent = "Remove from favorites";
-          }
-
-          // Update the favorites list or perform other actions as needed
-          updateFavoritesList();
-        });
-
         movieHeadingElements.appendChild(movieTitleElement);
-        movieHeadingElements.appendChild(favoritesButton);
         movieInfoContainer.appendChild(movieHeadingElements);
         movieInfoContainer.appendChild(movieContext);
         movieContext.appendChild(moviePoster);
@@ -124,64 +86,6 @@ document.addEventListener("DOMContentLoaded", function () {
         movieContext.appendChild(movieCastElement);
       })
       .catch((error) => console.error("Error fetching movie data:", error));
-  }
-
-  function getFavorites() {
-    // Retrieve favorites from local storage
-    localStorage.clear();
-    return localStorage.getItem("favorites")
-      ? localStorage.getItem("favorites").split(",")
-      : [];
-  }
-
-  function setFavorites(favorites) {
-    // Store favorites in local storage
-    localStorage.setItem("favorites", favorites.join(","));
-  }
-
-  function updateFavoritesList() {
-    const favoritesContainer = document.getElementById("favoritesContainer");
-    favoritesContainer.style.display = "flex";
-    // Clear previous favorites
-    favoritesContainer.innerHTML = "";
-
-    // Retrieve favorites from local storage
-    const favorites = getFavorites();
-
-    if (favorites.length > 0) {
-      const favoritesTitle = document.createElement("h2");
-      favoritesTitle.textContent = "Favorite Movies";
-      favoritesContainer.appendChild(favoritesTitle);
-
-      const favoritesList = document.createElement("ul");
-
-      // Create list items for each favorite movie
-      favorites.forEach((favorite) => {
-        // Split the favorite string into title and poster using a delimiter (e.g., "|")
-        const [title, poster] = favorite.split("|");
-
-        const listItem = document.createElement("li");
-
-        // Display the title
-        const titleElement = document.createElement("h3");
-        titleElement.textContent = title;
-        listItem.appendChild(titleElement);
-
-        // Display the poster
-        const posterElement = document.createElement("img");
-        posterElement.src = poster;
-        posterElement.alt = title;
-        listItem.appendChild(posterElement);
-
-        favoritesList.appendChild(listItem);
-      });
-
-      favoritesContainer.appendChild(favoritesList);
-    } else {
-      const noFavoritesMessage = document.createElement("p");
-      noFavoritesMessage.textContent = "No favorite movies yet.";
-      favoritesContainer.appendChild(noFavoritesMessage);
-    }
   }
 
   // Event listener for the search input
